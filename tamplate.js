a5300f560aa8b5050deca517d10a7dc5aff42462
MD Monirujjaman <monirbinkhaled1@gmail.com>
2024-10-10 16:53:10 +0600

    tamplate string,multiple line string and dynamic string done

--- tamplate.js
+++ tamplate.js
@@ -1,30 +1,35 @@
-// const a = 12;
-// const b = 54;
-// const d = 98;
-// console.log(result);
-
-// const s = 78;
-// const k = 90;
-
-
+const a = 12;
+const b = 54;
+const d = 98;
+const s = 78;
+const k = 90;
 const result1 = 164;
 const result2 = 168;
+const add = `${a+d}`;
+console.log(add);
+
 const sum = `${result1 + result2}`;
 // const result2 = `the sum of ${a} and ${b} is ${a+b}`;
 
-// console.log(sum);
+console.log(sum);
 
 const numbers =[45, 65, 75, 34, 65, 34];
 numbers [3] = 35;
+numbers [5] = 95;
 const student = {
   name : "monir",
   roll: 5,
   adderese: 'gazipur.'
 }
-const oparetion =`The numbers array of therd index is ${numbers[3]} and student object velue of adderes is ${student.adderese}`;
+const test = `${numbers[2] + student.roll} `;
+console.log(test);
+// const oparetion =`The numbers array of therd index is ${numbers[3]} and student object velue of adderes is ${student.adderese}`; 
+
+const oparetion =`The numbers array of last index is ${numbers[5]} and student object velue of adderes is ${student.name}`;
+
 console.log (oparetion);
 
 const email = `amar nam monir 
 ami gazipur thaki
 ami akjon student.`;
-// console.log(email);
+console.log(email);
